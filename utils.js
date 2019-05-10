a4bb1ef12571a720130a30fb6dc686d60f5d1585
方镇东 <fangzhendong@yiwise.com>
2019-05-10 18:28:34 +0800

    fix utils template path absolute

--- utils.js
+++ utils.js
@@ -1,10 +1,13 @@
 const fs = require('fs')
+const path = require('path')
 
 const prefixCompOrgEnum = [
   'yiwise',
   'el'
 ]
-const compTypesEnum = fs.readdirSync('./templates')
+
+const templatesDirPath = path.resolve(__dirname, './templates/')
+const compTypesEnum = fs.readdirSync(templatesDirPath)
   .map(item => item.replace(/\.js/, ''))
   .filter(item => item !== 'export')
 
